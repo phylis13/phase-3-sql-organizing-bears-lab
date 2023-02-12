2f634ab7994876773c9c0e117d71f6df913040f1
phylis13 <phylisnjeri54@gmail.com>
2023-02-12 21:52:33 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr.Chocolate", 2, "M", "Black", "wild", "true");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 1, "M", "Brown", "cool", "true");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 2,"F", "Black", "wild", "false");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 6, "M", "Brown", "wild", "true");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 3, "F", "Black", "wild",  "false");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 4, "M", "Dark brown", "cool", "true");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 1, "F", "Black", "wild", "true");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (Null, 3, "F", "White", "wild",  "false");
